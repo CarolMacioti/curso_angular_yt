3fbd810dae5b7a657cf0efa0280df2ff690f3164
Carolina <carolmacioti.dev@gmail.com>
2024-05-13 13:51:45 -0300

    Dynamic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
+
+/* Cada PATH representa uma rota */
+/* :id é o comando p\ criar uma rota dinâmica, quero, com ele, dizer que essa rota irá mudar cada vez que for acessada */
 
 const routes: Routes = [
-  {
-    path: '',
-    component: FirstComponentComponent,
-  } /* Cada PATH representa uma rota */,
+  { path: '', component: FirstComponentComponent },
   { path: 'list', component: ListRenderComponent },
+  { path: 'list/:id', component: ItemDetailComponent },
 ];
 
 @NgModule({
